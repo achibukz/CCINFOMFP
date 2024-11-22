8b3047cdff71863ba98e2a3c180c02522c4b760d
Chy <chyn22tria@gmail.com>
2024-11-22 23:23:47 +0800

    Updated SQL Commands for prescriptions table

--- prescriptions.sql
+++ prescriptions.sql
@@ -1,26 +1,27 @@
 --Create new presc record--
-INSERT INTO prescriptions (p.presId)
-VALUES (%s);
-
-INSERT INTO customers (customerId, customerLastName, customerFirstName)
-VALUES (%s, %s, %s);
-
-INSERT INTO medicines (medId, name)
-VALUES(%s, %s)
-
-INSERT INTO doctors (docId, doctorLastName, doctorFirstName)
-VALUES (%s, %s, %s);
-
---Reading the customer record to check if the customer is existing or not--
-SELECT * 
-FROM customers
-WHERE customerId = %s;
+INSERT INTO prescriptions (presId, customerId, medId, docId)
+VALUES (%s, %s, %s, %s);
 
 --Updating the prescription to change to a new prescription of a customer--
 UPDATE prescriptions p
-JOIN customers c ON p.customerId = c.customerId
-SET p.medId = %s
-WHERE p.presId = %s AND c.customerFirstName = %s AND c.customerLastName = %s;
+JOIN medSup m ON m.medId = p.medId
+SET p.medName = %s
+WHERE p.presId = %s;
+
+UPDATE prescriptions p
+JOIN medSup m ON m.medId = p.medId
+SET p.dosage = %s
+WHERE p.presId = %s;
+
+UPDATE prescriptions p
+JOIN doctors d ON d.docId = p.docId
+SET p.doctorFirstName = %s
+WHERE p.presId = %s;
+
+UPDATE prescriptions p
+JOIN doctors d ON d.docId = p.docId
+SET p.doctorLastName = %s
+WHERE p.presId = %s;
 
 --Deleting the prescription record of a customer if not or no longer valid--
 DELETE FROM prescriptions
@@ -29,14 +30,41 @@ WHERE presId = %s;
 --Prescription record Report--
 SELECT 
     p.presId,
-    c.customerId,
     CONCAT(c.customerLastName, ', ', c.customerFirstName) AS CustomerName,
-    m.medId,
-    m.name,
+    m.medName, 
+    m.dosage,
     CONCAT(d.doctorLastName, ', ', d.doctorFirstName) AS DoctorName,
-FROM Prescription p
-JOIN customers c ON p.customerID = c.customerID
-JOIN medicines m ON p.medId = m.medId
-JOIN doctors d ON p.docID = d.docID
-WHERE YEAR(p.dateIssued) = %s AND MONTH(p.dateIssued) = %s;
+FROM prescriptions p
+JOIN customers c ON p.customerId = c.customerId
+JOIN medSup m ON p.medId = m.medId
+JOIN doctors d ON p.docId = d.docId
+WHERE YEAR(p.dateIssued) = %s AND MONTH(p.dateIssued) = %s
+ORDER BY presId;
 
+
+--SQL commands for viewing tables--
+SELECT DISTINCT CONCAT(c.customerLastName,', ', c.customerFirstName) AS CustomerName
+FROM prescriptions p
+JOIN customers c ON c.customerId = p.customerId;
+
+SELECT presId
+FROM prescriptions
+JOIN customers c ON c.customerId = p.customerId
+WHERE c.customerFirstName = (SELECT customerFirstName
+                              FROM prescriptions)
+  AND c.customerLastName = (SELECT customerLastName
+                              FROM prescriptions);
+
+SELECT DISTINCT m.medName
+FROM prescriptions p
+LEFT JOIN medSup m ON m.medId = p.medId;
+
+SELECT m.dosage
+FROM prescriptions p
+JOIN medSup m ON m.medId = p.medId
+WHERE m.medName = (SELECT medName 
+                FROM prescriptions);
+
+SELECT DISTINCT CONCAT(d.doctorLastName, ', ', d.doctorFirstName) AS DoctorName
+FROM prescriptions p
+LEFT JOIN doctors d ON d.docID = p.docID;
